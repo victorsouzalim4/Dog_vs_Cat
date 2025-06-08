2899e396ba3bffd7c893f956f23b495245801bd6
Victor <victorsouzalima2064@gmail.com>
2025-06-08 19:06:05 -0300

    test implemented

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 from Utils.treat_data import treat_data_set
 from Utils.split_data_set import split_dataset
 from CNN.generate_model import gen_model
+from tensorflow.keras.models import load_model
+from sklearn.metrics import classification_report, confusion_matrix
+from keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
 
-train_path = "archive/splitted_data/train"
-val_path = "archive/splitted_data/val"
+
+
+
+
 test_path = "archive/splitted_data/test"
-
-train_generator = treat_data_set(train_path)
-val_generator = treat_data_set(val_path)
 test_generator = treat_data_set(test_path)
 
-model = gen_model()
-
-model.compile(
-    optimizer='adam',
-    loss='binary_crossentropy',
-    metrics=['accuracy']
-)
-
-model.fit(
-    train_generator,
-    validation_data=val_generator,
-    epochs=10,
-    steps_per_epoch=len(train_generator),
-    validation_steps=len(val_generator)
-)
+model = load_model("Model/dog_vs_cat_model_final.keras")
 
 test_loss, test_acc = model.evaluate(test_generator)
 print(f"Test Accuracy: {test_acc:.4f} | Test Loss: {test_loss:.4f}")
+
+y_pred_probs = model.predict(test_generator)
+y_pred = (y_pred_probs > 0.5).astype(int)
+
+y_true = test_generator.classes
+
+print(classification_report(y_true, y_pred, target_names=test_generator.class_indices.keys()))
+
+cm = confusion_matrix(y_true, y_pred)
+
+# Nomes das classes (ordenados pelo índice 0, 1)
+labels = list(test_generator.class_indices.keys())
+
+# Exibir matriz de confusão
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+
+plt.figure(figsize=(6, 6))
+disp.plot(cmap=plt.cm.Blues, values_format='d')
+plt.title("Matriz de Confusão - Conjunto de Teste")
+plt.show()
+
+
